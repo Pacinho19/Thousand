5c87daf9c71a0a3fbb5a8d7291fe431c8fc9c9a3
Pacinho19 <pacio170@interia.pl>
2023-03-29 12:58:04 +0200

    Initial commit

---
+++ src/main/java/pl/pacinho/thousand/ThousandApplication.java
@@ -0,0 +1,13 @@
+package pl.pacinho.thousand;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThousandApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThousandApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/pacinho/thousand/ThousandApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.pacinho.thousand;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThousandApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
